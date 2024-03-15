6cf413cc5807b3b29bdaa53db23d0d5181cce2bf
Lautaro Beber <lautarobeber5a@gmail.com>
2024-03-14 22:55:35 -0300

    triyin using sockets commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen('https://globalsocketchat.netlify.app/');
+  await app.listen('3000');
 }
 bootstrap();
